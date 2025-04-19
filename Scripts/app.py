d06071308016a3af66dfef065a7b2bb780becd7b
Kawakami Takuma <kumasan@maslab.aitech.ac.jp>
2025-04-19 17:15:34 +0900

    Update app.py
    main関数の作成
    　データのアップロード機能
    　アップロードしたデータの可視化

--- Scripts/app.py
+++ Scripts/app.py
@@ -3,3 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as pt
 import seaborn as sns
 
+def main():
+    uploded_file = st.file_uploader("csvファイルをアップロードしてください", type = "csv")
+    if uploded_file is not None:
+        df = pd.read_csv(uploded_file)
+        st.write(df)
+
+if __name__ == "__main__":
+    main()
