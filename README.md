2856f55e6a7ccd28c6561ae9f33121c8d2c5eee7
Kawakami Takuma <kumasan@maslab.aitech.ac.jp>
2025-04-21 20:27:30 +0900

    add function for plot_hist
    ヒストグラムをプロットする関数
    プロットするデータの選択と実行ボタンの作成

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## 使い方
 1. venv_DataPlotの仮想環境をアクティベート
 2. streamlit run app.py
+
+## フォルダ構成
--- Scripts/app.py
+++ Scripts/app.py
@@ -3,11 +3,28 @@ import pandas as pd
 import matplotlib.pyplot as pt
 import seaborn as sns
 
+option = ["ヒストグラム", "散布図", "箱ひげ図"]
+
 def main():
     uploded_file = st.file_uploader("csvファイルをアップロードしてください", type = "csv")
     if uploded_file is not None:
         df = pd.read_csv(uploded_file)
         st.write(df)
+        
+        
+        graph = st.selectbox("プロットするグラフを選んでください", options = option)
+        if graph == option[0]: # ヒストグラム
+            plot_hist(df)
+
+def plot_hist(df): # とりあえず１つだけ
+    # ToDo 複数選択可能にしたい
+    data = st.selectbox("データを選択してください", options = df.columns)
+    plot_button = st.button("実行")
+    if plot_button is True:
+        st.write(pt.hist(data, bins=20)) # ヒストグラムプロットできず
+
+
+
 
 if __name__ == "__main__":
     main()
